7bebc4437b07cf832b44d85da96912b7ff7cfc9e
Derek McDaniel <>
2021-03-06 01:38:10 -0700

    basic canvas drawing

--- js/draw.js
+++ js/draw.js
@@ -1,18 +1,32 @@
 
-window.addEventListener("load", DrawInit);
+window.addEventListener("load", Init);
 
 let Ui = {};
 let State = {};
 
 function Page(){
   EZ.clear();
-  canvas = EZ("canvas");
-  Ui.canvas = canvas({style: "position: absolute; top: 1; left: 1; border: 1px solid red;");
-  EZ.append(Ui.canvas);
+  EZ.append(
+    EZ("canvas")(
+      {id: 'outcanvas', style: "position: absolute; top: 0; left: 0; border: none;"})());
+  Ui.canvas = EZ.get('outcanvas');
   Ui.ctx = Ui.canvas.getContext("2d");
-  ctx.fillRect(60,60,80,20);
+  Resize();
+  Draw();
+}
+function Resize(){
+  Ui.canvas.width = window.innerWidth;
+  Ui.canvas.height = window.innerHeight;
 }
 
-function DrawInit(){
+function Draw(){
+  Ui.ctx.clearRect(0, 0, Ui.canvas.width, Ui.canvas.height);
+  Ui.ctx.fillRect(0, 0, Ui.canvas.width, Ui.canvas.height);
+  Ui.ctx.clearRect(4, 4, Ui.canvas.width-8, Ui.canvas.height-8);
+  Ui.ctx.fillRect(30,40,50,60); 
+}
+
+function Init(){
   Page();
+  window.addEventListener('resize', Resize);
 }
